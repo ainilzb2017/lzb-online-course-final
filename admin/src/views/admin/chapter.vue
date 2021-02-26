219e91bb7a6e043cbd9a501107f75b11c0e31d94
ainilzb2017 <479134309@qq.com>
2021-02-26 17:16:31 +0800

    设置激活样式并实现页面跳转

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -972,3 +972,15 @@
     </table>
 </template>
 
+<script>
+    export default {
+        name: "chapter",
+        mounted: function() {
+            this.$parent.activeSidebar("business-chapter-sidebar");
+        },
+        methods: {
+
+        }
+    }
+</script>
+
