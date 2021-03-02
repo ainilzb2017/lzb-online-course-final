47044219f74e879a0fb5756ac37870e2e2eeba74
ainilzb2017 <479134309@qq.com>
2021-03-02 10:01:37 +0800

    生成器开发：增加生成service层的代码

---
+++ server/src/main/java/com/ainilzb/server/service/SectionService.java
@@ -0,0 +1,70 @@
+package com.ainilzb.server.service;
+
+import com.ainilzb.server.domain.Section;
+import com.ainilzb.server.domain.SectionExample;
+import com.ainilzb.server.dto.SectionDto;
+import com.ainilzb.server.dto.PageDto;
+import com.ainilzb.server.mapper.SectionMapper;
+import com.ainilzb.server.util.CopyUtil;
+import com.ainilzb.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class SectionService {
+
+    @Resource
+    private SectionMapper sectionMapper;
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        SectionExample sectionExample = new SectionExample();
+        List<Section> sectionList = sectionMapper.selectByExample(sectionExample);
+        PageInfo<Section> pageInfo = new PageInfo<>(sectionList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<SectionDto> sectionDtoList = CopyUtil.copyList(sectionList, SectionDto.class);
+        pageDto.setList(sectionDtoList);
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(SectionDto sectionDto) {
+        Section section = CopyUtil.copy(sectionDto, Section.class);
+        if (StringUtils.isEmpty(sectionDto.getId())) {
+            this.insert(section);
+        } else {
+            this.update(section);
+        }
+    }
+
+    /**
+     * 新增
+     */
+    private void insert(Section section) {
+        section.setId(UuidUtil.getShortUuid());
+        sectionMapper.insert(section);
+    }
+
+    /**
+     * 更新
+     */
+    private void update(Section section) {
+        sectionMapper.updateByPrimaryKey(section);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id) {
+        sectionMapper.deleteByPrimaryKey(id);
+    }
+}
