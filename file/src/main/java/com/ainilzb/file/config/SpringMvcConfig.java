230774dbc0acbf00d988b65c6f4d151ba676adad
ainilzb2017 <479134309@qq.com>
2021-03-11 15:00:33 +0800

    文件上传功能开发：讲师头像显示优化

--- file/src/main/java/com/ainilzb/file/config/SpringMvcConfig.java
+++ file/src/main/java/com/ainilzb/file/config/SpringMvcConfig.java
@@ -1,5 +1,6 @@
 package com.ainilzb.file.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
+    @Value("${file.path}")
+    private String FILE_PATH;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/file/ainilzb/course/");
+        registry.addResourceHandler("/f/**").addResourceLocations("file:"  + FILE_PATH);
     }
 
     //http://127.0.0.1:9003/file/f/teacher/kmO4zTEP-five.jpg
-    //http://127.0.0.1:9000/file/D:/file/ainilzb/course/teacher/kmO4zTEP-five.jpg
 }
