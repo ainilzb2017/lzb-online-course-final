b52a61e9a8d4f1b323790e05663803718d256353
ainilzb2017 <479134309@qq.com>
2021-03-11 10:53:01 +0800

    增加file模块，用于文件上传和存储

---
+++ file/src/main/java/com/ainilzb/file/config/FileApplication.java
@@ -0,0 +1,28 @@
+package com.ainilzb.file.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Slf4j
+@ComponentScan("com.ainilzb")
+@MapperScan("com.ainilzb.server.mapper")
+public class FileApplication {
+
+
+	public static void main(String[] args) {
+		//SpringApplication.run(FileApplication.class, args);
+
+		SpringApplication app = new SpringApplication(FileApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		log.info("启动成功！！");
+		log.info("File地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+}
