eabd2cf8ebd10067a97e283fd86ebf742e9f920e
ainilzb2017 <479134309@qq.com>
2021-03-12 11:21:03 +0800

    文件上传功能开发：文件表设计与基本代码生成

---
+++ server/src/main/java/com/ainilzb/server/enums/FileUseEnum.java
@@ -0,0 +1,32 @@
+package com.ainilzb.server.enums;
+
+public enum FileUseEnum {
+
+    COURSE("C", "课程"),
+    TEACHER("T", "讲师");
+
+    private String code;
+
+    private String desc;
+
+    FileUseEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
---
+++ server/src/main/java/com/ainilzb/server/service/FileService.java
@@ -0,0 +1,75 @@
+package com.ainilzb.server.service;
+
+import com.ainilzb.server.domain.File;
+import com.ainilzb.server.domain.FileExample;
+import com.ainilzb.server.dto.FileDto;
+import com.ainilzb.server.dto.PageDto;
+import com.ainilzb.server.mapper.FileMapper;
+import com.ainilzb.server.util.CopyUtil;
+import com.ainilzb.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Date;
+
+@Service
+public class FileService {
+
+    @Resource
+    private FileMapper fileMapper;
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto) {
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
+        FileExample fileExample = new FileExample();
+        List<File> fileList = fileMapper.selectByExample(fileExample);
+        PageInfo<File> pageInfo = new PageInfo<>(fileList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<FileDto> fileDtoList = CopyUtil.copyList(fileList, FileDto.class);
+        pageDto.setList(fileDtoList);
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(FileDto fileDto) {
+        File file = CopyUtil.copy(fileDto, File.class);
+        if (StringUtils.isEmpty(fileDto.getId())) {
+            this.insert(file);
+        } else {
+            this.update(file);
+        }
+    }
+
+    /**
+     * 新增
+     */
+    private void insert(File file) {
+        Date now = new Date();
+        file.setCreatedAt(now);
+        file.setUpdatedAt(now);
+        file.setId(UuidUtil.getShortUuid());
+        fileMapper.insert(file);
+    }
+
+    /**
+     * 更新
+     */
+    private void update(File file) {
+        file.setUpdatedAt(new Date());
+        fileMapper.updateByPrimaryKey(file);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id) {
+        fileMapper.deleteByPrimaryKey(id);
+    }
+}
