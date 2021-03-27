34b2235050994be02371e3e58f4da81588f3ad97
samli <479134309@qq.com>
2021-03-27 15:21:40 +0800

    1. 课程列表页面开发，根据点击的分类进行课程筛选 动态sql不只可以进行动态列查询，也可以进行动态表关联

--- server/src/main/java/com/ainilzb/server/mapper/my/MyCourseMapper.java
+++ server/src/main/java/com/ainilzb/server/mapper/my/MyCourseMapper.java
@@ -1,9 +1,15 @@
 package com.ainilzb.server.mapper.my;
 
+import com.ainilzb.server.dto.CourseDto;
+import com.ainilzb.server.dto.CoursePageDto;
 import com.ainilzb.server.dto.SortDto;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface MyCourseMapper {
+
+    List<CourseDto> list(@Param("pageDto") CoursePageDto pageDto);
 
     int updateTime(@Param("courseId") String courseId);
 
